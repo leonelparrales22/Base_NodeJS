80a75dc9f2f5a3ae566503682751e84f84b9f9b8
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-04-08 14:31:58 -0500

    clase

---
+++ 06.-WebServer/app.js
@@ -0,0 +1,19 @@
+const http = require("http");
+http
+  .createServer((req, res) => {
+    res.write("HolaMundo");
+    res.end();
+  })
+  .listen(8081);
+
+console.log("Escuchando el puerto 8081");
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+}
+
+var a = 5;
+console.log(a);
+
+xd = true;
+a
