5fad2068a51759346aa71da1b24bb88596c45c33
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-04-08 18:22:11 -0500

    clase

--- 06.-WebServer/server.js
+++ 06.-WebServer/server.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const app = express();
+const hbs = require("hbs");
 
 app.use(express.static(__dirname + "/public"));
 
-// app.get("/", (req, res) => {
-//   let salida = {
-//     nombre: "Leonel",
-//     edad: 22,
-//     url: req.url,
-//   };
-//   res.send(salida);
-// });
+//Express HBS engine
+hbs.registerPartials(__dirname + "/views/partials");
+
+require("./hbs/helpers");
+
+app.set("view engine", "hbs");
+
+//helpers
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    nombre: "leonel parrales",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {});
+});
 
 app.get("/data", (req, res) => {
   res.send("Hola Data");
