9d296929b4aeb57e82b1b85b80f93343ea51204a
Leonel <dparrales@est.ups.edu.ec>
2020-04-17 15:54:08 -0500

    clase

---
+++ IngeTufiño/03.-bases/app.js
@@ -0,0 +1,22 @@
+const { crearArchivo } = require("./multiplicar/multiplicar");
+const argv = require("yargs").command(
+  "listar",
+  "Imprime en consola la tabla de multiplicar.",
+  {
+    base: {
+      demand: true,
+      alias: "b",
+    },
+    limite: {
+      alias: "l",
+      default: 10,
+    },
+  }
+).argv;
+
+console.log(argv.base, argv.limite);
+
+// let base = process.argv[2].split("=")[1];
+// crearArchivo(base)
+//   .then((mensaje) => console.log(mensaje))
+//   .catch((err) => console.log(err));
